9335bc47ccf52852d57c39a9527d57934ac2864b
TranDat <trandat251111@gmail.com>
2022-02-16 14:54:25 +0700

    add1

---
+++ Circle/Circle.java
@@ -0,0 +1,33 @@
+package Circle;
+public class Circle {
+    double radius;
+    String color;
+    boolean filled;
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
